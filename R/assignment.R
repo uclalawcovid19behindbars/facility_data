43578530af46160657b04dedc8868b594f1f5dd4
erika-tyagi <erika.tyagi@gmail.com>
2020-12-07 09:34:01 -0600

    Added simple fn to generate new ID

--- R/assignment.R
+++ R/assignment.R
@@ -1,10 +1,15 @@
 library(tidyverse)
 
-# Create new Count.ID 
-# df should be fac_data or fac_spellings 
-generate_new_id <- function(df) {
-  max_id <- max(df$Count.ID)
+# Load data 
+fac_data <- read_csv("./data_sheets/fac_data.csv", col_types = cols())
+fac_alt <- read_csv("./data_sheets/fac_spellings.csv", col_types = cols())
+
+# Generate new Count.ID 
+generate_new_id <- function() {
+  max_id <- max(fac_data$Count.ID, fac_alt$Count.ID, na.rm = TRUE)
   new_id <- max_id + 1
   return(new_id)
 }
+
+
 
