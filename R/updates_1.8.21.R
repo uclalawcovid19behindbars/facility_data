67cfc31732a1c142c23a8977134940d8053313fd
erika-tyagi <erika.tyagi@gmail.com>
2021-01-11 12:32:57 -0600

    Almost done??

--- R/updates_1.8.21.R
+++ R/updates_1.8.21.R
@@ -12,19 +12,45 @@ fac_data <- fac_data %>%
     mutate(Security = case_when(Security == "Max/Mid" ~ "Max/Med", 
                                 TRUE ~ Security), 
            Is.Different.Operator = case_when(Is.Different.Operator ~ 1, 
-                                             is.na(Is.Different.Operator) ~ 0))
+                                             is.na(Is.Different.Operator) ~ 0)) %>% 
+    rename("Population.Feb20" = "Population", 
+           "Source.Population.Feb20" = "Source.Population")
 
 fac_spellings <- fac_spellings %>% 
-    rename("facility_name_raw" = "Name.Raw", 
-           "facility_name_clean" = "Name.Clean") %>% 
-    select(-Jurisdiction)
+    rename("xwalk_name_raw" = "Name.Raw", 
+           "xwalk_name_clean" = "Name.Clean") %>% 
+    select(-Jurisdiction) %>% 
+    distinct()
     
 # Add IDs to info sheet 
 fac_data <- fac_data %>% 
     arrange(State, Name) %>% 
     group_by(State, Name) %>%
     mutate(Facility.ID = cur_group_id()) %>% 
-    select(Facility.ID, everything())
+    select(Facility.ID, 
+           State, 
+           Name, 
+           Jurisdiction, 
+           Description, 
+           Security, 
+           Age, 
+           Gender, 
+           Is.Different.Operator, 
+           Different.Operator, 
+           Population.Feb20, 
+           Capacity, 
+           HIFLD.ID, 
+           BJS.ID, 
+           Source.Population.Feb20, 
+           Source.Capacity, 
+           Address, 
+           City, 
+           Zipcode, 
+           Latitude, 
+           Longitude, 
+           County, 
+           County.FIPS, 
+           Website) 
 
 # Sanity check - should be true 
 stopifnot(max(fac_data$Facility.ID) == nrow(fac_data))
@@ -33,26 +59,27 @@ stopifnot(max(fac_data$Facility.ID) == nrow(fac_data))
 # Federal facilities: merge on cleaned name 
 federal <- fac_spellings %>% 
     filter(Is.Federal == 1) %>% 
-    mutate(name_clean_ = clean_fac_col_txt(facility_name_clean, to_upper = TRUE)) %>% 
+    mutate(name_clean_ = clean_fac_col_txt(xwalk_name_clean, to_upper = TRUE)) %>% 
     select(-State) %>% 
     left_join(fac_data %>% 
                   filter(Jurisdiction == "federal") %>%  
                   mutate(name_clean_ = clean_fac_col_txt(Name, to_upper = TRUE)),  
               by = "name_clean_") %>% 
-    select(Facility.ID, facility_name_raw, facility_name_clean, State, Is.Federal)
+    select(Facility.ID, xwalk_name_raw, xwalk_name_clean, State, Is.Federal)
 
 # Non-federal facilities: merge on cleaned name + state 
 non_federal <- fac_spellings %>% 
     filter(Is.Federal == 0) %>% 
-    mutate(name_clean_ = clean_fac_col_txt(facility_name_clean, to_upper = TRUE)) %>% 
+    mutate(name_clean_ = clean_fac_col_txt(xwalk_name_clean, to_upper = TRUE)) %>% 
     left_join(fac_data %>% 
                   filter(Jurisdiction != "federal") %>% 
                   mutate(name_clean_ = clean_fac_col_txt(Name, to_upper = TRUE)),
               by = c("name_clean_", "State")) %>% 
-    select(Facility.ID, facility_name_raw, facility_name_clean, State, Is.Federal)
+    select(Facility.ID, xwalk_name_raw, xwalk_name_clean, State, Is.Federal)
 
 full_spellings <- bind_rows(federal, non_federal) %>% 
-    arrange(State, Facility.ID)
+    arrange(State, Facility.ID) %>% 
+    distinct()
 
 stopifnot(nrow(full_spellings) == nrow(fac_spellings))
 stopifnot(nrow(full_spellings) == nrow(distinct(full_spellings)))
