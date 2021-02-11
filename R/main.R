40e60e43123454b37c861808594cf6aacf94755d
hjohns12 <hopecaneel@gmail.com>
2021-02-11 15:39:51 -0500

    add NM jails finally

--- R/main.R
+++ R/main.R
@@ -10,7 +10,7 @@ OLD_FAC_SPELLINGS <- read_fac_spellings()
 HIFLD_DATA <- read_hifld_data() 
 
 # Specify Google Sheet URL here! 
-url <- "1oTTKeyiPPXJnjNI5ZzxQFlsb6G49xk-VUSacZPwNCGI"
+url <- "1DuaobAABkA8JtcqLghYrjkcHYTsA7pAfBKCdG-34NME"
 
 # ------------------------------------------------------------------------------
 # Update facility info sheet 
