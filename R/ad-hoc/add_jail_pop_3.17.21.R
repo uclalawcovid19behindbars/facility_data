4b81872b29ce60ef42cd2f744d2c02db221f0b51
erika-tyagi <erika.tyagi@gmail.com>
2021-03-18 08:26:30 -0500

    fix: add feb20 population source

--- R/ad-hoc/add_jail_pop_3.17.21.R
+++ R/ad-hoc/add_jail_pop_3.17.21.R
@@ -10,21 +10,29 @@ updated_fac_info <- old_fac_info %>%
     rows_update(tibble(Facility.ID = 312, 
                        Name = "ST LUCIE COUNTY JAIL")) %>% 
     rows_update(tibble(Facility.ID = 58, 
-                       Population.Feb20 = 5306)) %>% 
+                       Population.Feb20 = 5306, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 135, 
-                       Population.Feb20 = 17076)) %>% 
+                       Population.Feb20 = 17076, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 144, 
-                       Population.Feb20 = 5074)) %>% 
+                       Population.Feb20 = 5074, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 312, 
-                       Population.Feb20 = 808)) %>% 
+                       Population.Feb20 = 808, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 1579, 
-                       Population.Feb20 = 1519)) %>% 
+                       Population.Feb20 = 1519, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 1726, 
-                       Population.Feb20 = 3279)) %>% 
+                       Population.Feb20 = 3279, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 1759, 
-                       Population.Feb20 = 4780)) %>% 
+                       Population.Feb20 = 4780, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) %>% 
     rows_update(tibble(Facility.ID = 2075, 
-                       Population.Feb20 = 1163)) 
+                       Population.Feb20 = 1163, 
+                       Source.Population.Feb20 = "Vera Jail Population Data")) 
 
 updated_fac_spellings <- old_fac_spellings %>% 
     mutate(xwalk_name_clean = case_when(Facility.ID == 312 ~ "ST LUCIE COUNTY JAIL", 
